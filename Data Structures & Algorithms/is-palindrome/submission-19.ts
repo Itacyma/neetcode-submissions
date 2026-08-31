1a9734bd29cbef400e5d1e0a4374572c80e184f7
Claudio Martini <marclaudio03@gmail.com>
2026-09-01 01:02:11 +0200

    Add: is-palindrome - submission-19

---
+++ Data Structures & Algorithms/is-palindrome/submission-19.ts
@@ -0,0 +1,31 @@
+class Solution {
+    isPalindrome(s: string): boolean {
+        let l = 0;
+        let r = s.length - 1;
+
+        while (l < r) {
+            while (l < r && !this.isAlphaNumeric(s[l])) {
+                l++
+            }
+            while (l < r && !this.isAlphaNumeric(s[r])) {
+                r--
+            }
+
+            // Se i puntatori non si sono incrociati, confronta i caratteri in minuscolo
+            if (s[l].toLowerCase() !== s[r].toLowerCase()) {
+                return false
+            }
+
+            l++
+            r--
+        }
+
+        return true
+    }
+
+    private isAlphaNumeric(c: string): boolean {
+        return (c >= 'a' && c <= 'z') ||
+            (c >= 'A' && c <= 'Z') ||
+            (c >= '0' && c <= '9');
+    }
+}
