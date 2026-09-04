26bd0838a7cd34c6596b9f1be85a922a5fcffe60
Claudio Martini <marclaudio03@gmail.com>
2026-09-04 17:20:23 +0200

    Add: minimum-stack - submission-7

---
+++ Data Structures & Algorithms/minimum-stack/submission-7.ts
@@ -0,0 +1,48 @@
+class MinStack {
+    private stack: number[]
+    private minStack: number[]
+
+    constructor() {
+        this.stack = []
+        this.minStack = []
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val: number): void {
+        this.stack.push(val)
+        if(this.minStack.length == 0){
+            this.minStack.push(val) 
+            return
+        }
+
+        if(val < this.minStack[this.minStack.length -1])
+            this.minStack.push(val) 
+        else 
+            this.minStack.push(this.minStack[this.minStack.length -1]) 
+    }
+
+    /**
+     * @return {void}
+     */
+    pop(): void {
+        this.minStack.pop()
+        this.stack.pop()
+    }
+
+    /**
+     * @return {number}
+     */
+    top(): number {
+        return this.stack[this.stack.length-1]
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin(): number {
+        return this.minStack[this.minStack.length-1]
+    }
+}
