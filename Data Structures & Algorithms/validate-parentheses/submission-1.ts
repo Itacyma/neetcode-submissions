08af9c1731d8e7815ee995f866035d89b7f9fa6d
Claudio Martini <marclaudio03@gmail.com>
2026-09-04 16:33:40 +0200

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s: string): boolean {
+        let queue: string[] = []
+        let openParentesi = "{[("
+        let closeParentesi = "}])"
+
+        for(let char of s){
+            if(openParentesi.includes(char)){
+                queue.push(char)
+            }
+            else if (closeParentesi.includes(char)){
+                let last = queue.pop()
+                if(!((last == '(' && char == ')') || (last == '[' && char == ']') || (last == '{' && char == '}')))
+                    return false
+            }
+        }
+        return queue.length == 0
+    }
+}
